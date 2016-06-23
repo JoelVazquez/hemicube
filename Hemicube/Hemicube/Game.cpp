18246b2d9383194a9a647af9f65e5afb94d70ea0
GuillermoBaez <solidsnake798@gmail.com>
2016-06-23 15:59:40 -0300

    Add standard

--- Hemicube/Hemicube/Game.cpp
+++ Hemicube/Hemicube/Game.cpp
@@ -1,130 +1,17 @@
 #include "Game.h"
 
-GLuint VertexArrayID;
-GLuint programID;
-glm::mat4 MVP, MVP2;
-GLuint MatrixID;
-GLuint vertexbuffer;
-GLuint colorbuffer;
-void State1::enter(Game* game)
-{
-	this->game = game;
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	glEnable(GL_DEPTH_TEST);
-	// Accept fragment if it closer to the camera than the former one
-	glDepthFunc(GL_LESS);
-
-
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	// Create and compile our GLSL program from the shaders
-	programID = LoadShaders("../TransformVertexShader.vertexshader", "../ColorFragmentShader.fragmentshader");
-
-	// Get a handle for our "MVP" uniform
-	MatrixID = glGetUniformLocation(programID, "MVP");
-
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-		);
-
-	glm::mat4 View2 = glm::lookAt(
-		glm::vec3(4, -3, -3), // Camera is at (4,3,-3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-		);
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
-	MVP2 = Projection * View2 * Model;
-	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	
-	
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-	// 1rst attribute buffer : vertices
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(
-		0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-		3,                  // size
-		GL_FLOAT,           // type
-		GL_FALSE,           // normalized?
-		0,                  // stride
-		(void*)0            // array buffer offset
-		);
-
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-	// 2nd attribute buffer : colors
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(
-		1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-		3,                                // size
-		GL_FLOAT,                         // type
-		GL_FALSE,                         // normalized?
-		0,                                // stride
-		(void*)0                          // array buffer offset
-		);
-	// Use our shader
-	glUseProgram(programID);
-
-};
-
-void State1::update()
-{
-	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Draw the triangle !
-	glBindVertexArray(VertexArrayID);
-	glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles
-
-};
-
-void State1::leave()
-{
-
-};
-
-void State2::enter(Game* game)
-{
-	this->game = game;
-};
-
-void State2::update()
-{
-	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP2[0][0]);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Draw the triangle !
-	glBindVertexArray(VertexArrayID);
-	glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles
-};
-
-void State2::leave()
-{
-
-};
 
 Game::Game()
 {
-	current_state = new State1();
-	current_state->enter(this);
+	current_state = new PlayingState(this);
 }
 
 void Game::update(double &lag)
 {
 	while (lag >= MS_PER_UPDATE)
 	{
-		//current_state->update();
+		current_state->Update();
 		lag -= MS_PER_UPDATE;
 	}
 }
@@ -136,9 +23,9 @@ void Game::handleKey(int &key, int &action)
 	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
 		std::cout << "handleKey: UP press" << std::endl;
 	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
-		this->current_state = new State1();
-	if (key == GLFW_KEY_2 && action == GLFW_PRESS)
-		this->current_state = new State2();
+		this->current_state = new PlayingState(this);
+	//if (key == GLFW_KEY_2 && action == GLFW_PRESS)
+		//this->current_state = new State2();
 }
 
 void Game::handleCursorPos(double &xpos, double &ypos)
@@ -148,7 +35,7 @@ void Game::handleCursorPos(double &xpos, double &ypos)
 
 void Game::render()
 {
-	current_state->update();
+	current_state->Render();
 	//std::cout << "render" << std::endl;
 }
 
---
+++ Hemicube/Hemicube/IGameState.h
@@ -0,0 +1,12 @@
+#ifndef I_GAME_STATE_H
+#define I_GAME_STATE_H
+class Game;
+class IGameState
+{
+public:
+	virtual ~IGameState() {};
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+};
+
+#endif
